7eabca34486e8f038b74a9f88fa34c0cf1075c83
Alex Barik <floyd.nice95@gmail.com>
2024-04-15 15:40:10 +0300

    Initial commit

---
+++ src/test/java/com/example/weather_spring_boot_starter/WeatherSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
